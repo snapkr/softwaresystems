552fc3c3495d876b911bd83596cd33c34e358ea1
S A Parker <mailseanparker+service@gmail.com>
2015-12-03 16:01:59 +0100

    fixed DoublyLinkedList

--- src/ss/week4/DoublyLinkedList.java
+++ src/ss/week4/DoublyLinkedList.java
@@ -2,69 +2,87 @@ package ss.week4;
 
 public class DoublyLinkedList<Element> {
 
-    private /*@ spec_public @*/ int size;
-    private Node head;
+	private /* @ spec_public @ */ int size;
+	private Node head;
 
-    //@ ensures this.size == 0;
-    public DoublyLinkedList() {
-        size = 0;
-        head = new Node(null);
-        head.next = head;
-        head.previous = head;
-    }
+	// @ ensures this.size == 0;
+	public DoublyLinkedList() {
+		size = 0;
+		head = new Node(null);
+		head.next = head;
+		head.previous = head;
+	}
 
-    //@ requires element != null;
-    //@ requires 0 <= index && index <= this.size;
-    //@ ensures this.size == \old(size) + 1;
-    //@ ensures this.getNode(index).equals(element);
-    public void add(int index, Element element) {
-        // TODO: implement, see exercise P-4.17
-    }
+	// @ requires element != null;
+	// @ requires 0 <= index && index <= this.size;
+	// @ ensures this.size == \old(size) + 1;
+	// @ ensures this.getNode(index).equals(element);
+	public void add(int index, Element element) {
+		Node newNode = new Node(element);
+		if (size >= 1) {
+			Node p = getNode(index - 1);
+			newNode.next = p.next;
+			newNode.next.previous = newNode;
+			newNode.previous = p;
+			p.next = newNode;
+		} else {
+			Node p = getNode(-1);
+			p.next = newNode;
+			p.previous = newNode;
+			newNode.next = p;
+			newNode.previous = p;
+		}
+		size++;
+	}
 
-    //@ requires 0 <= index && index < this.size;
-    //@ ensures this.size == \old(size) - 1;
-    public void remove(int index) {
-        // TODO: implement, see exercise P-4.17
-    }
+	// @ requires 0 <= index && index < this.size;
+	// @ ensures this.size == \old(size) - 1;
+	public void remove(int index) {
+		Node pastNode = getNode(index);
+		pastNode.previous.next = pastNode.next;
+		pastNode.next.previous = pastNode.previous;
+		size--;
+	}
 
-    //@ requires 0 <= index && index < this.size;
-    /*@ pure */ public Element get(int index) {
-        Node p = getNode(index);
-        return p.element;
-    }
+	// @ requires 0 <= index && index < this.size;
+	/* @ pure */ public Element get(int index) {
+		Node p = getNode(index);
+		return p.element;
+	}
 
-    /**
-     * The node containing the element with the specified index.
-     * The head node if the specified index is -1.
-     */
-    //@ requires -1 <= i && i < this.size;
-    //@ pure
-    public Node getNode(int i) {
-        Node p = head;
-        int pos = -1;
-        while (pos < i) {
-            p = p.next;
-            pos = pos + 1;
-        }
-        return p;
-    }
+	/**
+	 * The node containing the element with the specified index. The head node
+	 * if the specified index is -1.
+	 */
+	// @ requires -1 <= i && i < this.size;
+	// @ pure
+	public Node getNode(int i) {
+		Node p = head;
+		int pos = -1;
+		while (pos < i) {
+			p = p.next;
+			pos = pos + 1;
+		}
+		return p;
+	}
 
-    public int size() {
-        return this.size;
-    }
-    public class Node {
-        public Node(Element element) {
-            this.element = element;
-            this.next = null;
-            this.previous = null;
-        }
+	public int size() {
+		return this.size;
+	}
 
-        private Element element;
-        public Node next;
-        public Node previous;
+	public class Node {
+		public Node(Element element) {
+			this.element = element;
+			this.next = null;
+			this.previous = null;
+		}
 
-        public Element getElement() {
-            return element;
-        }
-    }
+		private Element element;
+		public Node next;
+		public Node previous;
+
+		public Element getElement() {
+			return element;
+		}
+	}
 }
--- src/ss/week4/test/PolynomialTest.java
+++ src/ss/week4/test/PolynomialTest.java
@@ -5,25 +5,48 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import ss.week4.math.LinearProduct;
+import ss.week4.math.Polynomial;
+
 public class PolynomialTest {
+
+	private static final double DELTA = 1e-15;
+	//private static final double[] CONSTANT_VALUE = new double[] {0,1,2,3};
+	private static final double[] CONSTANT_VALUE = new double[] {7,9,3,6};
+	private Polynomial polynomial;
 
 	@Before
 	public void setUp() throws Exception {
+		polynomial = new Polynomial(CONSTANT_VALUE);
 	}
 
-	@Test
-	public void testPolynomial() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testApply() {
-		fail("Not yet implemented");
+		/*
+		assertEquals(0, polynomial.apply(0), DELTA);
+		assertEquals(-2, polynomial.apply(-1), DELTA);
+		assertEquals(6, polynomial.apply(1), DELTA);
+		*/
+		assertEquals(877, polynomial.apply(5), DELTA);
+		assertEquals(25, polynomial.apply(1), DELTA);
+		
 	}
 
 	@Test
 	public void testDerivative() {
-		fail("Not yet implemented");
+		/*
+		//assertTrue(polynomial.derivative() instanceof Polynomial);
+		assertEquals(0.0, polynomial.derivative().apply(0), DELTA);
+		//assertEquals(, polynomial.derivative().apply(1), DELTA);
+		 */
+		assertEquals(489, polynomial.derivative().apply(5), DELTA);
+		assertEquals(33, polynomial.derivative().apply(1), DELTA);
+	}
+
+	@Test
+	public void testIntegrand(){
+
 	}
 
 }
